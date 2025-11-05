0474db78ed1e811846bee5c00e9f1ce18db6fcc6
Tweenitwits <tweenitwits@gmail.com>
2025-11-05 15:17:19 +0300

    renamed wrong file

--- __TEST/main.go
+++ __TEST/main.go
@@ -2,12 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-	a := "scissors"
-	b := "paper"
-	c := "rock"
+func create(iterations int) []int {
+	a := make([]int, 0)
+	for i := 0; i < iterations; i++ {
+		a = append(a, i)
+	}
+	return a
+}
 
-	fmt.Println(a > b)
-	fmt.Println(b > c)
-	fmt.Println(a > c)
+func main() {
+	sliceFromLoop()
+	sliceFromLiteral()
+
+}
+
+func sliceFromLoop() {
+	fmt.Printf("** NOT working as expected: **\n\n")
+	i := create(11)
+	fmt.Println("initial slice: ", i)
+	j := append(i, 100)
+	g := append(i, 101)
+	h := append(i, 102)
+	fmt.Printf("i: %v\nj: %v\ng: %v\nh:%v\n", i, j, g, h)
+}
+
+func sliceFromLiteral() {
+	fmt.Printf("\n\n** working as expected: **\n")
+	i := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("initial slice: ", i)
+	j := append(i, 100)
+	g := append(i, 101)
+	h := append(i, 102)
+	fmt.Printf("i: %v\nj: %v\ng: %v\nh:%v\n", i, j, g, h)
 }
