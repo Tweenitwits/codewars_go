13df4ac007a5610102d158cbcfb96b1b1d6defeb
Tweenitwits <tweenitwits@gmail.com>
2025-11-25 10:35:52 +0300

    thirteenth

---
+++ Volume of a Cuboid/main.go
@@ -0,0 +1,14 @@
+/*
+Description:
+Bob needs a fast way to calculate the volume of
+a rectangular cuboid with three values: the length,
+width and height of the cuboid.
+
+Write a function to help Bob with this calculation.
+*/
+
+package kata
+
+func GetVolumeOfCuboid(length, width, height float64) float64 {
+	return length * width * height
+}
