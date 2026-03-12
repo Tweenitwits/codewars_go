15fb7bd3e33babba2a2e127986ca7406a1ec0327
Tweenitwits <tweenitwits@gmail.com>
2026-03-12 18:46:56 +0300

    CSV

--- 8kyu/CSV_representation_of_array/main.go
+++ 8kyu/CSV_representation_of_array/main.go
@@ -23,8 +23,35 @@ Note: you shouldn't escape the \n, it should work as a new line.
 
 package main
 
-func ToCsvText(array [][]int) string {
-	for i, row := range array {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func ToCsvText(array [][]int) string {
+	addElem := "\\n"
+	resultStr := ""
+	lastIndex := len(array) - 1
+	for i, row := range array {
+		stringsRow := []string{}
+		for _, val := range row {
+			tmp := strconv.Itoa(val)
+			stringsRow = append(stringsRow, tmp)
+		}
+		s := strings.Join(stringsRow, ",")
+		if i != lastIndex {
+			resultStr += s + addElem
+		} else {
+			resultStr += s
+		}
 	}
+	return resultStr
+}
+
+func main() {
+	fmt.Println(ToCsvText([][]int{{0, 1, 2, 3, 4},
+		{10, 11, 12, 13, 14},
+		{20, 21, 22, 23, 24},
+		{30, 31, 32, 33, 34}}))
 }
